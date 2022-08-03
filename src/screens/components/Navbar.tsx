8cc9641e2e72e87bccf16f0295493b1e0ab764d0
Saurabh <srvkmr130@gmail.com>
2022-08-03 05:51:26 +0530

    mark selected tab

--- src/screens/components/Navbar.tsx
+++ src/screens/components/Navbar.tsx
@@ -3,33 +3,58 @@ import { NavLink } from "react-router-dom";
 import "./styles/navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import logo from "../../resources/brand-logo.png";
+import { TAB } from "../../utils/enums";
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
+  const [tabOption, setTabOption] = useState(TAB.HOME);
+
+  const handleTabChange = (tab: TAB) => {
+    setTabOption(tab);
+  };
+
   const menuOptions = useMemo(
     () => (
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" onClick={() => handleTabChange(TAB.HOME)}>
+            Home
+          </NavLink>
+          <div className={tabOption === TAB.HOME ? "selected" : ""}></div>
         </li>
         <li>
-          <NavLink to="/product">Products</NavLink>
+          <NavLink to="/product" onClick={() => handleTabChange(TAB.PRODUCTS)}>
+            Products
+          </NavLink>
+          <div className={tabOption === TAB.PRODUCTS ? "selected" : ""}></div>
         </li>
         <li>
-          <NavLink to="/awards">Awards</NavLink>
+          <NavLink to="/awards" onClick={() => handleTabChange(TAB.AWARDS)}>
+            Awards
+          </NavLink>
+          <div className={tabOption === TAB.AWARDS ? "selected" : ""}></div>
         </li>
         <li>
-          <NavLink to="/media">Media</NavLink>
+          <NavLink to="/media" onClick={() => handleTabChange(TAB.MEDIA)}>
+            Media
+          </NavLink>
+          <div className={tabOption === TAB.MEDIA ? "selected" : ""}></div>
         </li>
         <li>
-          <NavLink to="/contacts">Contacts</NavLink>
+          <NavLink to="/contacts" onClick={() => handleTabChange(TAB.CONTACTS)}>
+            Contacts
+          </NavLink>
+          <div className={tabOption === TAB.CONTACTS ? "selected" : ""}></div>
         </li>
         <li>
-          <NavLink to="/career">Career</NavLink>
+          <NavLink to="/career" onClick={() => handleTabChange(TAB.CAREER)}>
+            Career
+          </NavLink>
+          <div className={tabOption === TAB.CAREER ? "selected" : ""}></div>
         </li>
       </ul>
     ),
-    []
+    [tabOption]
   );
   return (
     <nav className="main-nav">
